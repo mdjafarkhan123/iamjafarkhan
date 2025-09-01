71aa311f247d65a2e68536d787949b6083b0bb3b
Jafar Khan <profile.mdjafarkhan@gmail.com>
2025-09-01 16:01:12 +0600

    Cta rolling text fixed

--- src/code/lenis.js
+++ src/code/lenis.js
@@ -1,11 +1,10 @@
 import Lenis from "lenis";
 const lenis = new Lenis({
-    wheelMultiplier: 1.5,
+    wheelMultiplier: 2,
     smoothWheel: true,
 });
 function raf(time) {
     lenis.raf(time);
     requestAnimationFrame(raf);
 }
-
 requestAnimationFrame(raf);
